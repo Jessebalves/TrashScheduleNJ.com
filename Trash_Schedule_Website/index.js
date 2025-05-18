23b9c9fd23e4cbf0bc0bc69d34859a62a015eba8
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-05-17 23:32:23 -0400

    Add files via upload

--- Trash_Schedule_Website/index.js
+++ Trash_Schedule_Website/index.js
@@ -1,8 +1,3 @@
-//window.alert("This is a warning message!");
-
-//document.getElementById("my_h1").textContent = "This is a header defined in JavaScript"; 
-
-
 const date = new Date(); 
 
 //Dictionary - Not sure if this will be used
@@ -15,15 +10,12 @@ console.log(myDictionary);
 //day of the week 0-6 (Ex. Monday is 1)
 const number_day = date.getDay();
 //Actual day of the month (Ex. the 12th)
-let month_day = date.getDate()
+let month_day = date.getDate();
 //Month number
-let month = date.getMonth(); 
+let month = date.getMonth()+1; 
 //Year number
 const year = date.getFullYear();
 
-//test numbers for holiday
-//month = 7; 
-//month_day = 4; 
 
 const full_date = (String(month)+ "/" + String(month_day)+ "/" + String(year));
 
@@ -34,18 +26,31 @@ console.log(month_day)
 console.log(year);
  
 console.log("Current date:", full_date);
+document.getElementById("displayed_date").textContent= "Current Date: "+ full_date;
 
-//button function
-document.getElementById("mySubmit").onclick = function(){
-    let street_address = document.getElementById("address").value;
+//Associated with changing table elements to "H"
+const holiday_dates = document.querySelectorAll('#testo');
+holiday_dates.forEach(holiday_dates => {
+    holiday_dates.textContent = "H";
+    console.log("This should appear 3 times");
+})
+
+//function associated with form on index.html
+document.getElementById('test_server').addEventListener('submit', async (a) => {
+    a.preventDefault();
+    const street_address = document.getElementById('address').value;
     console.log(street_address);
-    document.getElementById("com2").textContent="Address Entered: "+street_address; 
-    document.getElementById("cmon").textContent= "Current Date: "+ full_date;
-    if (month == 1 && month_day == 1) {
-        console.log("New Years Eve");
-        console.log(number_day);
-        document.getElementById("com3").textContent="It is New Years Eve. No Trash for today.";
-		}
+})
+
+
+    //document.getElementById("com2").textContent="Address Entered: "+street_address; 
+    //document.getElementById("cmon").textContent= "Current Date: "+ full_date;
+    
+ //   if (month == 1 && month_day == 1) {
+ //       console.log("New Years Eve");
+  //      console.log(number_day);
+ //       document.getElementById("com3").textContent="It is New Years Eve. No Trash for today.";
+//		}
 
     // Memorial Day(Test This)
     //else if (month == 5 && number_day == 2 && week_of_month == 4) {
@@ -53,9 +58,9 @@ document.getElementById("mySubmit").onclick = function(){
     //	}
 
     // 4th of July / Independence Day (Good)
-    else if (month == 7 && month_day == 4) {
-        document.getElementById("com3").textContent="It is 4th of July. No Trash for today.";
-		}
+//    else if (month == 7 && month_day == 4) {
+  //      document.getElementById("com3").textContent="It is 4th of July. No Trash for today.";
+		
 	// Labor day, month = september, number_day = monday, rando = week of month
 	// (Test This)
     //else if (month == 9 && number_day == 2 && week_of_month == 1) {
@@ -66,7 +71,6 @@ document.getElementById("mySubmit").onclick = function(){
     //	Console.log("Happy Thanksgiving");
 //		}
     // Christmas (Good)
-    else if (month == 12 && month_day == 25) {
-	    console.log("Christmas");
-		}
-}
+ //   else if (month == 12 && month_day == 25) {
+//	    console.log("Christmas");
+//
