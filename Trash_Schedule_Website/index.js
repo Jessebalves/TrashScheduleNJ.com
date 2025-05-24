03a4782d905e86db75d5dc0e0d6d23f466f4db6a
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-05-24 11:23:30 -0400

    Added week of month variable
    Please test this

--- Trash_Schedule_Website/index.js
+++ Trash_Schedule_Website/index.js
@@ -1,7 +1,7 @@
 const date = new Date(); 
 let isHoliday = false; 
 
-//Dictionary
+//Map (Numbers, Days of the Week)
 let dayMap = new Map();
 dayMap.set(0, "Sunday");
 dayMap.set(1, "Monday");
@@ -17,17 +17,25 @@ let number_day = 5;
 
 //Actual day of the month (Ex. the 12th)
 //let month_day = date.getDate();
-let month_day = 4; 
+let month_day = 1; 
 
 //Month number (Ex. March is 3)
 //let month = date.getMonth()+1; 
-let month = 7; 
+let month = 1; 
 
 //Year number (Ex. 2025)
 const year = date.getFullYear();
 
 //Combining all the date variables to create a Full date string (Ex. 4/12/2025)
 const full_date = (String(month)+ "/" + String(month_day)+ "/" + String(year));
+
+///var d = new Date();
+//var datey = 26;
+//var day = 5;
+
+//Variable associated with the week of the given month
+var weekOfMonth = Math.ceil((month_day - 1 - month) / 7)+1;
+console.log("Week of the month:", weekOfMonth);
 
 //This will be printed in the console on JavaScript
 console.log("Month: "+month)
@@ -37,6 +45,8 @@ console.log("Year: "+year);
 console.log("Current date:", full_date);
 document.getElementById("displayed_date").textContent= "Current Date: "+ full_date;
 
+//This checks if the current date is a holiday
+//If it is a holiday, is Holiday will be True, otherwise, it is False 
 //New Years Eve
 if (month == 1 && month_day == 1) {
           console.log("New Years Eve");
@@ -65,6 +75,7 @@ if (month == 1 && month_day == 1) {
     	console.log("Happy Thanksgiving");
         isHoliday = true;
 		}
+    
     // Christmas (Good)
    else if (month == 12 && month_day == 25) {
         console.log("Christmas");
@@ -73,6 +84,7 @@ if (month == 1 && month_day == 1) {
 
 console.log("Checking if it is a holiday: "+isHoliday);
 
+//If it is a Holiday, this will mark table entries associated with that day with an "X"
 if(isHoliday == true){
     let day_found = dayMap.get(number_day);
     let new_string = "#"+ day_found + "";
