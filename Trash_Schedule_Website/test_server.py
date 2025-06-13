ef784049dcde7fc83b6cde680724dcc582b0b1ff
Jessebalves <152413074+Jessebalves@users.noreply.github.com>
2025-06-13 03:20:44 -0400

    Updated python file
    The python file can now retrieve information from the mytestdb database, which consists of street name, low range, high range, side, zipcode, and ward number.

--- Trash_Schedule_Website/test_server.py
+++ Trash_Schedule_Website/test_server.py
@@ -1,32 +1,47 @@
 #imports
 #used to send recieve requests and send back data
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template # type: ignore
+from flask_cors import CORS # type: ignore
 #Connects test_server.py to MYSQL database on
 #MYSQL workbench
-import mysql.connector
+import mysql.connector # type: ignore
 
 
 #This is code that connects us to our MYSQL database
+#These are the credentials of the MYSQL server we are connecting this python file to 
 dataBase = mysql.connector.connect(
     host='localhost',
     user = 'root',
-    password = 'Rando15*'
+    password = 'Rando15*',
+    database = 'mytestdb'
     )    
 
 cursor = dataBase.cursor()
 cursor.execute("show databases")
+#myresult = cursor.fetchall()
 
 print("Connection to MYSQL database running on Localhost...")
 for x in cursor:
     print("Database found: ",x)
 print("\n")
 
+cursor.execute("SELECT * FROM addresses")
+myresult = cursor.fetchall()
+#print(myresult)
+for x in myresult: 
+    if x[0] == "ELM ST":
+        print("Street Name: ", x[0])
+        print("Low Range: ", x[1])
+        print("High Range: ",x[2])
+        print("Side: ",x[3])
+        print("Zipcode: ",x[4])
+        print("Ward:",x[-1])
+        print("\n")
 
 app = Flask(__name__)
 CORS(app)
 
-    
+#This section of code is executed when you press the "check trash" button on the front end
 @app.route('/data', methods=['POST'])
 def handle_data():
     data = request.get_json()
@@ -44,6 +59,17 @@ def handle_data():
         print("The address supplied was: ",value)
         print("\n")
         random_variable = value
+    
+    for x in myresult: 
+        if x[0] == value:
+            print("Street Name: ", x[0].lower())
+            print("Low Range: ", x[1])
+            print("High Range: ",x[2])
+            print("Side: ",x[3])
+            print("Zipcode: ",x[4])
+            print("Ward:",x[-1])
+            print("\n")
+
 
     return jsonify(random_variable)
 
